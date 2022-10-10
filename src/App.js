67f65f4183cdbac21d8693f03d06c00397e5394b
ellyrose <elly_turnbull@hotmail.co.uk>
2022-10-10 16:43:39 +0100

    Add initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import nameData from "./nameData"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const nameElements = nameData.map(name => {
+        return <p>{name}</p>
+    })
+    return (
+        <div>
+            {nameElements}
+        </div>
+    )
 }
 
-export default App;
